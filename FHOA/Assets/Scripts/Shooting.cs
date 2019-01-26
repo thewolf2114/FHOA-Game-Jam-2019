055ce36239f2ded7b0200f70a725df670aa8cb51
Samuel Layton <36344172+SamLayton97@users.noreply.github.com>
2019-01-25 20:48:24 -0700

    Added stupid shooting

---
+++ FHOA/Assets/Scripts/Shooting.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Basic shooting script for firing bullets from a gun
+/// </summary>
+public class Shooting : MonoBehaviour
+{
+    // public variables
+    public GameObject bulletPrefab;     // bullet which agent fires
+    public float bulletVelocity = 10f;  // velocity at which bullet fires
+    public float fireRate = .666f;      // time between shots
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if player is firing
+        if (Input.GetAxisRaw("Fire1") != 0)
+        {
+            // create instance of bullet
+            GameObject newBullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            newBullet.GetComponent<Rigidbody>().AddForce(Camera.main.transform.forward * bulletVelocity, ForceMode.Impulse);
+        }
+    }
+}
