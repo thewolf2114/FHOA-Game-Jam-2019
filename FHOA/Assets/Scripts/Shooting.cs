7e51b8cbdb40c2e8e937bbb23235e93dc91d507e
Samuel Layton <36344172+SamLayton97@users.noreply.github.com>
2019-01-25 20:56:28 -0700

    implemented fire rate

--- FHOA/Assets/Scripts/Shooting.cs
+++ FHOA/Assets/Scripts/Shooting.cs
@@ -12,15 +12,34 @@ public class Shooting : MonoBehaviour
     public float bulletVelocity = 10f;  // velocity at which bullet fires
     public float fireRate = .666f;      // time between shots
 
+    // private variables
+    bool canShoot = true;               // flag determining whether player can fire bullet on given frame
+    float fireFrameCounter = 0;         // counter aiding in fire rate
+
     // Update is called once per frame
     void Update()
     {
+        // if counter exceeds time between shots
+        if (fireFrameCounter >= fireRate)
+        {
+            // reset can shoot flag and fire rate counter
+            canShoot = true;
+            fireFrameCounter = 0;
+        }
+        // otherwise (counter has not exceeded limit)
+        else
+            // increment timer with time between frames
+            fireFrameCounter += Time.deltaTime;
+
         // if player is firing
-        if (Input.GetAxisRaw("Fire1") != 0)
+        if (Input.GetAxisRaw("Fire1") != 0 && canShoot)
         {
             // create instance of bullet
             GameObject newBullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
             newBullet.GetComponent<Rigidbody>().AddForce(Camera.main.transform.forward * bulletVelocity, ForceMode.Impulse);
+
+            // set gun to unable to fire
+            canShoot = false;
         }
     }
 }
