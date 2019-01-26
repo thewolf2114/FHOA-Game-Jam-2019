428d0f52c0fae5864f777c7ca9004b8e9a5230b7
Samuel Layton <36344172+SamLayton97@users.noreply.github.com>
2019-01-25 21:54:03 -0700

    partial implementation of gun action

--- FHOA/Assets/Scripts/Shooting.cs
+++ FHOA/Assets/Scripts/Shooting.cs
@@ -10,12 +10,24 @@ public class Shooting : MonoBehaviour
     // public variables
     public GameObject bulletPrefab;     // bullet which agent fires
     public GameObject bulletSpawnPoint; // point at which to spawn bullets from
+    public GameObject gunBarrel;        // barrel sub-object of the gun
+    public GameObject gunAction;        // action of gun that slides back on fire
     public float bulletVelocity = 10f;  // velocity at which bullet fires
     public float fireRate = .666f;      // time between shots
 
     // private variables
     bool canShoot = true;               // flag determining whether player can fire bullet on given frame
     float fireFrameCounter = 0;         // counter aiding in fire rate
+    Vector3 actionStartPosition;        // starting position of gun action
+    Vector3 actionFirePosition;         // position of gun action when gun fires a bullet
+
+    // Called a frame before first Update()
+    void Start()
+    {
+        // save initial and firing positions of gun action
+        actionStartPosition = gunAction.transform.position;
+        actionFirePosition = new Vector3(0, 0, 0);
+    }
 
     // Update is called once per frame
     void Update()
@@ -38,6 +50,9 @@ public class Shooting : MonoBehaviour
             // create instance of bullet
             GameObject newBullet = Instantiate(bulletPrefab, bulletSpawnPoint.transform.position, Quaternion.identity);
             newBullet.GetComponent<Rigidbody>().AddForce(Camera.main.transform.forward * bulletVelocity, ForceMode.Impulse);
+
+            // move action back to fire position
+            gunAction.transform.position = actionFirePosition;
 
             // set gun to unable to fire
             canShoot = false;
