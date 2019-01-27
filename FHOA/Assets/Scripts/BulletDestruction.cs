e758dd7b03241ceacdc0b2d9f2bc43c40e97814e
Samuel Layton <36344172+SamLayton97@users.noreply.github.com>
2019-01-26 18:16:01 -0700

    bullets destroy themselves when they either collide with another object or expire after set time

---
+++ FHOA/Assets/Scripts/BulletDestruction.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Handles destruction of projectile after a set period of time
+/// or collision.
+/// </summary>
+public class BulletDestruction : MonoBehaviour
+{
+    // public variables
+    public float projectileLifespan;        // time before a bullet destroys itself
+
+    // private variables
+    float lifeCounter = 0;                  // counter incrementing age of bullet (before it dies)
+
+    // Update is called once per frame
+    void Update()
+    {
+        // increment life counter
+        lifeCounter += Time.deltaTime;
+
+        // if counter exceeds max time, destroy bullet
+        if (lifeCounter >= projectileLifespan)
+        {
+            Destroy(gameObject);
+            Debug.Log("time death");
+        }
+    }
+
+    /// <summary>
+    /// When projectile object collides with another object
+    /// </summary>
+    /// <param name="collision">collider of the other object</param>
+    void OnCollisionEnter(Collision collision)
+    {
+        // destroy projectile
+        Destroy(gameObject);
+        Debug.Log("collision death");
+    }
+}
